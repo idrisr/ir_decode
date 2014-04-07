f11777122c6a13520ad1f858eeed9cbf847cfad8
Idris Raja <idris@viglink.com>
2014-04-06 22:48:25 -0700

    fix printing from subprocess

--- src/make_funcs.py
+++ src/make_funcs.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
+
 import glob
 import sys
 import os
+from subprocess import Popen, PIPE, STDOUT
 import subprocess
 
 dir = sys.argv[1]
@@ -8,7 +11,11 @@ dir_file = os.path.join(dir, '*')
 
 for file_ in glob.glob(dir_file):
     if os.path.isfile(file_) and file_.find('test') == -1:
-        print 'void Send%s(){' % (file_, )
+        filename = file_.split('/')[-1]
+        print 'void Send%s(){' % (filename, )
+
         command = "./ir_code_average.py %s" % (file_, )
-        os.system(command)
+        p = Popen(command, stdout=PIPE, stderr=STDOUT, shell=True)
+        stdout, stderr = p.communicate()
+        print stdout
         print '};'
